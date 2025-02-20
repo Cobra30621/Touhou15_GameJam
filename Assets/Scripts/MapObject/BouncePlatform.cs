ad5da716c72777af4ac0e4b8d4833d5e124e76d3
Cobra 陳俊宇 <cobra20621@gmail.com>
2025-02-21 00:55:38 +0800

    feat: add bounce Mushroom

---
+++ Assets/Scripts/MapObject/BouncePlatform.cs
@@ -0,0 +1,23 @@
+﻿namespace MapObject
+{
+    using UnityEngine;
+
+    public class BouncePlatform : MonoBehaviour
+    {
+        public float bounceForce = 10f; // 彈跳力度
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player")) // 確保是玩家踩到
+            {
+                Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (rb != null && rb.velocity.y <= 0) // 只在玩家向下落時觸發
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, 0); // 清除原本 Y 軸速度
+                    rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
+
+}
