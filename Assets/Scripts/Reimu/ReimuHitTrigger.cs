c190e029ec149360ad67ea08eac02a5a6202f2f5
Cobra 陳俊宇 <cobra20621@gmail.com>
2025-02-22 01:54:47 +0800

    fix: some reimu bugs

---
+++ Assets/Scripts/Reimu/ReimuHitTrigger.cs
@@ -0,0 +1,17 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Reimu
+{
+    public class ReimuHitTrigger : MonoBehaviour
+    {
+        [Required]
+        [SerializeField] private ReimuBattle _reimuBattle;
+
+        public void OnHit()
+        {
+            _reimuBattle.OnHit();
+        }
+        
+    }
+}
