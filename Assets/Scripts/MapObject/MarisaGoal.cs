294137adc4e1892e6ee0a095460fa461df1ed1ab
newyen2 <wish13love43xz@gmail.com>
2025-02-23 17:13:35 +0800

    Marisa Home

---
+++ Assets/Scripts/MapObject/MarisaGoal.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Player;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace MapObject
+{
+    public class MarisaGoal : MonoBehaviour
+    {
+        
+        
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                GameManager.Instance.EndGame();
+            }
+        }
+
+
+       
+    }
+}
