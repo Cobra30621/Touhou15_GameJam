9d406902b264301b3172bd6faa8d87765ea225a9
newyen2 <wish13love43xz@gmail.com>
2025-02-23 22:18:58 +0800

    canva UI update Z shooting

--- Assets/Scripts/UI/addbullet_manager.cs
+++ Assets/Scripts/UI/addbullet_manager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class addbullet_mannger : MonoBehaviour
+public class addbullet_manager : MonoBehaviour
 {
     public GameObject floatingUIPrefab; // 指向 FloatingUI 預製物
     public Transform uiCanvas; // 指定 UI Canvas 作為父物件
@@ -14,6 +14,8 @@ public class addbullet_mannger : MonoBehaviour
 
         // **生成 UI 物件**
         GameObject newUI = Instantiate(floatingUIPrefab, uiCanvas);
+
+
         print(newUI.transform.position+"be");
         //newUI.transform.position = screenPosition;
         print(newUI.transform.position+ "af");
